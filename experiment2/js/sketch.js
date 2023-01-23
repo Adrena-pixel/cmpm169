a6a3ea10ba70754a1d86907bcdfdd4a6c923c411
Adrena-pixel <ywang682@ucsc.edu>
2023-01-24 06:57:46 +0800

    Cola Sprite Fanta

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -10,10 +10,13 @@
 // Globals
 let myInstance;
 let canvasContainer;
-var numofbubbles = 20;
+var numofbubbles = 30;
 var bubbles = [numofbubbles];
 var ices = [];
 var additionalbubbles = [];
+var bgvalue = [[54,1,3],[189, 233, 203],[255,128,22]];
+var bubblevalue = [[173, 39, 46,95], [255,255,255,95],[255,209,16,95]]
+var colorpicker = 0;
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
@@ -30,6 +33,7 @@ function setup() {
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+    
     noStroke();
     for (let i = 0; i < numofbubbles; i++) {
         bubbles[i] = new Bubble();
@@ -38,12 +42,9 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    //background(0);    
-    // call a method on the instance
-    //myInstance.myMethod();
-
     // Put drawings here
-    background(0);
+    fill(bgvalue[colorpicker][0],bgvalue[colorpicker][1],bgvalue[colorpicker][2]);
+    rect(0, 0, windowWidth, windowHeight);
     for (let i = 0; i < bubbles.length; i++){
         bubbles[i].run();
     }
@@ -59,9 +60,27 @@ function mouseDragged(){
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
+function doubleClicked() {
     // code to run when mouse is pressed
     ices.push(new Ice());
+}
+function keyPressed() {
+  if (keyCode === LEFT_ARROW || keyCode === 65) {
+    if (colorpicker  == 0){
+      colorpicker = bgvalue.length - 1;
+    }
+    else{
+      colorpicker--;
+    }
+  } 
+  else if (keyCode === RIGHT_ARROW  || keyCode === 68) {
+    if (colorpicker == bgvalue.length - 1){
+      colorpicker = 0;
+    }
+    else{
+      colorpicker++;
+    }
+  }
 }
 
 //define my classes
@@ -111,6 +130,7 @@ class Bubble {
       }
     }
     update(){
+      fill(bubblevalue[colorpicker][0],bubblevalue[colorpicker][1],bubblevalue[colorpicker][2],bubblevalue[colorpicker][3]);
       circle (this.x,this.y,this.d);
     }
   }
@@ -144,6 +164,7 @@ class Bubble {
       }
     }
     update(){
+      fill(255,248,255, 98);
       square(this.x,this.y,this.size);
     }
   }
